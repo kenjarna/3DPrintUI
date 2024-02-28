e690600f7efc23b2d78b3e8e500bf3e99a0ae756
Kenny Jarnagin <jarnaginkenny@gmail.com>
2024-02-27 21:18:40 -0500

    Update CSS styling to match remix sidebar

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,15 @@
-import type { MetaFunction } from '@remix-run/node';
-import { Button } from '~/components/ui/button';
+import type {MetaFunction} from '@remix-run/node';
+import {Button} from '~/components/ui/button';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    {title: 'New Remix App'},
+    {name: 'description', content: 'Welcome to Remix!'},
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-
-    </div>
+    <div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.8'}}></div>
   );
 }
